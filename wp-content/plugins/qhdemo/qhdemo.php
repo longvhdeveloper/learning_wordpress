24fca125f5cf0802d457e265b35224acae4cfbb9
long vo <longvo@longs-MacBook-Air.local>
2015-08-17 15:58:58 +0700

    learning plugin in wordpress

---
+++ wp-content/plugins/qhdemo/qhdemo.php
@@ -0,0 +1,12 @@
+<?php
+/*
+    Plugin Name: QH Demo
+    Plugin URI:  http://wordpress.org/extend/plugins/xinchao/
+    Description: Day la plugin co ban
+    Version:     1.0
+    Author:      Jackie Wu
+    Author URI:  http://wordpress.org/extend/plugins/xinchao/
+    Text Domain: qhdemo
+    Domain path: /languages
+ */
+
